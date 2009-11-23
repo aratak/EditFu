80d20066bafec5198ef99c696dc158ac2b20a566
Sergey Kachan <sk@anahoret.com>
2009-11-23 15:47:51 +0200

    Fixed.

--- config/environment.rb
+++ config/environment.rb
@@ -41,4 +41,6 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
+
+  SITES_DIR = 'tmp/sites' unless defined? SITES_DIR
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,4 +27,4 @@ config.action_view.cache_template_loading            = true
 # Enable threaded mode
 # config.threadsafe!
 
-Site::SITES_DIR = '/var/edit-fu/sites'
+SITES_DIR = '/var/edit-fu/sites'
