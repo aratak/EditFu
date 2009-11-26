a034d35e0776d1354a9acc515514430c946ff8d1
Sergey Kachan <sk@anahoret.com>
2009-11-26 17:32:29 +0200

    Cleanup.

--- app/models/page.rb
+++ app/models/page.rb
@@ -6,7 +6,7 @@ class Page < ActiveRecord::Base
   def sections
     FtpClient.get(self)
 
-    (Hpricot(content) / '.editfu').map do |element|
+    map_sections(document) do |element|
       element.inner_html
     end
   end
@@ -14,12 +14,22 @@ class Page < ActiveRecord::Base
   def sections=(sections)
     i = 0
     document = Hpricot(content) 
-    (document / '.editfu').map do |element|
+    map_sections(document) do |element|
       element.inner_html = sections[i]
       i = i + 1
     end
     self.content = document.to_html
 
     FtpClient.put(self)
+  end
+
+  private
+
+  def document
+    Hpricot(content) 
+  end
+
+  def map_sections(document)
+    (document / '.editfu').map { |element| yield element }
   end
 end
