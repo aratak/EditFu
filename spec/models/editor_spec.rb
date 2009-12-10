3b5ed1dddfdc22737d12cf7a6d567016fb78b992
Sergey Kachan <sk@anahoret.com>
2009-12-10 09:10:35 +0200

    Added test on Editor.sites.

--- spec/models/editor_spec.rb
+++ spec/models/editor_spec.rb
@@ -1,26 +1,32 @@
 require 'spec_helper'
 
 describe Editor do
-  before :each do
-    @editor = Factory.build(:editor)
-    ActionMailer::Base.deliveries.clear
-  end
-
   describe 'save' do
     it "should deliver email instructions to new editor" do
-      @editor.save!
+      editor = Factory.build(:editor)
+      ActionMailer::Base.deliveries.clear
+      editor.save!
 
       ActionMailer::Base.deliveries.should_not be_empty
       email = ActionMailer::Base.deliveries.first
-      email.to.first.should == @editor.email
-      email.body.should match("invited by #{@editor.owner.email}")
+      email.to.first.should == editor.email
+      email.body.should match("invited by #{editor.owner.email}")
       email.body.should match("/editors/confirmation")
-      email.body.should match(@editor.confirmation_token)
+      email.body.should match(editor.confirmation_token)
     end
   end
 
   describe 'sites' do
     it "should return actual editor sites" do
+      editor = Factory.create(:editor)
+      site1 = Factory.create(:site, :owner => editor.owner)
+      site2 = Factory.create(:site, :owner => editor.owner)
+
+      editor.pages << Factory.create(:page, :site => site1)
+      editor.pages << Factory.create(:page, :site => site1)
+      editor.pages << Factory.create(:page, :site => site2)
+
+      editor.sites.should == [site1, site2]
     end
   end
 end
