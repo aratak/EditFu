e573af1b86f6d689e1e84ddf0911330e33ab528f
Sergey Kachan <sk@anahoret.com>
2010-03-12 10:29:28 +0200

    Removed unused cide from application.js.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,32 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-function adjustImage(image) {
-  var maxSize = parseInt(image.down('.thumbnail').getStyle('width'), 10);
-  var img = image.down('img');
-
-  img.originalHeight = img.height;
-  img.originalWidth = img.width;
-
-  if(img.height > img.width) {
-    img.height = maxSize;
-  } else {
-    img.width = maxSize;
-  }
-
-  var size = image.down('.size');
-  if(size) {
-    size.innerHTML = img.originalHeight + 'x' + img.originalWidth;
-  }
-
-  var title = image.down('.title');
-  if(title) {
-    title.innerHTML = title.innerHTML.truncate(20);
-  }
-
-  image.style.visibility = 'visible';
-}
-
 function showMessage(kind, text) {
   var message = clearMessage();
   if(text && !text.blank()) {
@@ -46,20 +20,11 @@ function clearMessage() {
   return message;
 }
 
-/* TODO: maybe it should be removed */
-function mainForm() {
-  return $('main').down('form');
-}
-
-Event.observe(window, 'load', function() {
-  
-  
-});
-
 Ajax.Responders.register({
     onCreate: function() {
       showMessage('info', 'Processing request...');
     },
+
     onComplete: function() {
       clearMessage();
     }
