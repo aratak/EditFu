a82ce1ddfa0deb14bc514603edb7b72c0a702535
Sergey Kachan <sk@anahoret.com>
2010-04-23 11:37:46 +0300

    Added html code popup.

--- public/javascripts/page.js
+++ public/javascripts/page.js
@@ -1,26 +1,53 @@
+function showImagePopup(ed) {
+  var el = ed.selection.getNode();
+  // Internal image object like a flash placeholder
+  if (el && ed.dom.getAttrib(el, 'class').indexOf('mceItem') != -1) {
+    return;
+  }
+
+  var imageAction;
+  if (!el || el.nodeName != 'IMG') {
+    window.editedImg = null;
+    imageAction = 'Insert';
+  } else {
+    window.editedImg = el;
+    imageAction = 'Edit';
+  }
+  window.editor = ed;
+  window.isSwapOut = false;
+
+  loadImagePopup({type: 'content', imageAction: imageAction});
+}
+
+function showHtmlCodePopup(ed) {
+  $('popup').innerHTML = $('code-popup-template').innerHTML;
+
+  if (tinymce.isGecko) {
+    document.body.spellcheck = ed.getParam("gecko_spellcheck");
+  }
+
+  var input = $('popup').down('textarea');
+  input.value = ed.getContent({source_view : true});
+  input.ed = ed;
+  showPopup();
+}
+
+function updateHtmlCode() {
+  var input = $('popup').down('textarea');
+  input.ed.setContent(input.value, {source_view : true});
+  hidePopup();
+}
+
 function initMceEditor(ed) {
-  ed.addCommand('efImage', function() {
-      var el = ed.selection.getNode();
-      // Internal image object like a flash placeholder
-      if (el && ed.dom.getAttrib(el, 'class').indexOf('mceItem') != -1) {
-        return;
-      }
-
-      var imageAction;
-      if (!el || el.nodeName != 'IMG') {
-        window.editedImg = null;
-        imageAction = 'Insert';
-      } else {
-        window.editedImg = el;
-        imageAction = 'Edit';
-      }
-      window.editor = ed;
-      window.isSwapOut = false;
-
-      loadImagePopup({type: 'content', imageAction: imageAction});
+  ed.addCommand('efImage', function() { 
+    showImagePopup(ed); 
   });
-
   ed.addButton('image', { title: 'Add Image', cmd: 'efImage' });
+
+  ed.addCommand('efCode', function() {
+    showHtmlCodePopup(ed);
+  });
+  ed.addButton('code', { title: 'HTML code', cmd: 'efCode' });
 }
 
 function editfuMceLayout(s, tb, o) {
@@ -36,7 +63,7 @@ function editfuMceLayout(s, tb, o) {
   }
   createToolbar('undo,redo', 'Left');
   createToolbar('formatselect,bold,italic,underline,bullist,numlist', 'Center');
-  createToolbar('image', 'Right');
+  createToolbar('image' + s.code, 'Right');
   o.deltaHeight -= s.theme_advanced_row_height;
 
   tr = DOM.add(tb, 'tr');
@@ -47,6 +74,7 @@ function editfuMceLayout(s, tb, o) {
 function initTinyMCE(settings) {
   tinyMCE.init($H(settings).merge({
     mode: "textareas",
+    editor_selector: 'mce_area',
     theme: "advanced",
     skin: "editfu",
     theme_advanced_layout_manager: "CustomLayout",
