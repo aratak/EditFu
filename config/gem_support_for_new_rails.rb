93f30fa4ccddb1bee54c68327600ec45a7991849
Alexey Osipenko <aratak@anahoret.com>
2010-05-13 15:54:47 +0300

    gem_support_for_new_rails added.

---
+++ config/gem_support_for_new_rails.rb
@@ -0,0 +1,10 @@
+if Gem::VERSION >= "1.3.6"
+  module Rails
+    class GemDependency
+      def requirement
+        r = super
+        (r == Gem::Requirement.default) ? nil : r
+      end
+    end
+  end
+end
