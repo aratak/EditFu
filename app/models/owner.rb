33e90bf23042ebfafaf0d878c079fa6678c6acab
Alexey Osipenko <aratak@anahoret.com>
2010-05-28 17:26:37 +0300

    validates_acceptance_of :terms_of_service added

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -5,7 +5,7 @@ class Owner < User
   
   before_validation_on_create :set_default_domain_name
 
-  attr_accessible :domain_name, :company_name
+  attr_accessible :domain_name, :company_name, :terms_of_service
 
   # Associations
   has_many :sites, :dependent => :destroy
@@ -20,6 +20,8 @@ class Owner < User
   validates_uniqueness_of :domain_name
   validates_format_of :domain_name, :with => /^\w+$/
   validates_exclusion_of :domain_name, :in => %w(www admin)
+
+  validates_acceptance_of :terms_of_service, :on => :create, :allow_nil => false, :message => 'Read and accept it!'
 
   # Methods
   def unlimited_trial=(val)
