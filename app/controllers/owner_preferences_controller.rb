4a7df262f360b2d8966e69c7d7c8fe9387103e34
Alexey Osipenko <aratak@anahoret.com>
2010-06-10 13:13:06 +0300

    owner_preferences/update action has been changed.

--- app/controllers/owner_preferences_controller.rb
+++ app/controllers/owner_preferences_controller.rb
@@ -9,39 +9,50 @@ class OwnerPreferencesController < ApplicationController
     @owner.require_current_password
     @owner.update_attributes(params[:preferences][:owner])
     @message = ['preferences.updated']
-
-    @plan = params[:preferences][:owner][:plan]
-    @card = ExtCreditCard.new params[:preferences][:card]
-
-    @plan_changed = @plan != @owner.plan
-    if @plan == 'professional' && (@plan_changed || !@card.number.blank?)
-      @card.valid?
-      if @owner.errors.empty? && @card.errors.empty?
-        begin
-          if @plan_changed
-            @message = ['plan.upgraded', {:plan_was => @owner.plan.capitalize}]
-            @owner.set_professional_plan @card
-          else
-            @owner.set_card @card
-          end
-        rescue PaymentSystemError
-          render_message I18n.t('plan.payment_error', 
-            :contact_us => MessageKeywords.contact_us('contact us'), 
-            :support => MessageKeywords.support_email)
-        end
-      end
+    
+    begin
+      @owner.set_plan(params[:preferences][:owner][:plan_id], params) if params[:preferences][:owner][:plan_id]
+    rescue PaymentSystemError
+      render_message I18n.t('plan.payment_error', 
+                            :contact_us => MessageKeywords.contact_us('contact us'), 
+                            :support => MessageKeywords.support_email)
     end
-
-    unless @owner.errors.empty? && @card.errors.empty?
-      render_errors :preferences_owner => @owner, :preferences_card => @card
-    end
+    
+    
+    
+    # @owner.require_current_password
+    # @owner.update_attributes(params[:preferences][:owner])
+    # @message = ['preferences.updated']
+    # 
+    # @plan = params[:preferences][:owner][:plan]
+    # @card = ExtCreditCard.new params[:preferences][:card]
+    #
+    # @plan_changed = @plan != @owner.plan
+    # if @plan == 'professional' && (@owner.plan_changed? || !@card.number.blank?)
+    #   @card.valid?
+    #   if @owner.errors.empty? && @card.errors.empty?
+    #     begin
+    #       if @owner.plan_changed?
+    #         @owner.set_plan(Plan::PROFESSIONAL, params)
+    #         @message = ['plan.upgraded', {:plan_was => @owner.plan_was.name}]
+    #       else
+    #         @owner.set_card @card
+    #       end
+    #     rescue PaymentSystemError
+    #       render_message I18n.t('plan.payment_error', 
+    #         :contact_us => MessageKeywords.contact_us('contact us'), 
+    #         :support => MessageKeywords.support_email)
+    #     end
+    #   end
+    # end
+    # 
+    # unless @owner.errors.empty? && @card.errors.empty?
+    #   render_errors :preferences_owner => @owner, :preferences_card => @card
+    # end
   end
 
   def downgrade
-    sites = pages = []
-    sites = Site.find(params[:sites]) if params[:sites]
-    pages = Page.find(params[:pages]) if params[:pages]
-    @owner.set_free_plan sites, pages
+    @owner.set_plan(Plan::FREE, params)
   end
   
   def identity
