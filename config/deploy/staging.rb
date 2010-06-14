f9ee81c083e2e8293214abd19bd7b5b8151189c1
Alexey Osipenko <aratak@anahoret.com>
2010-06-14 11:17:58 +0300

    Ditto.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,12 @@
 set :deploy_to, "/var/www/rails/#{application}/staging"
+
 # set :branch, "staging"
-set :tag, "staging"
+set :branch do
+  default_tag = `git tag`.split("\n").last
+
+  tag = Capistrano::CLI.ui.ask "Tag to deploy (make sure to push the tag first): [#{default_tag}] "
+  tag = default_tag if tag.empty?
+  tag
+end
+
 set :rails_env, "staging"
