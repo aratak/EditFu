3fe741ac26e9fcfc85045f731cc613c54d8fb644
Alexey Osipenko <aratak@anahoret.com>
2010-06-14 11:37:23 +0300

    capistrano changes: asking about branch into production environment. Foolproof.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,13 @@
 set :deploy_to, "/var/www/rails/#{application}/production"
 set :branch, "stable"
+
+set :branch do
+  default_branch = "stable"
+
+  branch = Capistrano::CLI.ui.ask "Branch to deploy (make sure to push the tag first): [#{default_tag}] "
+  branch = default_branch if branch.empty?
+  branch
+end
+
 set :rails_env, "production"
 
