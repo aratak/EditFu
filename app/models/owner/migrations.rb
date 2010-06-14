d88c1e77f9a0ea8c83a89d654379f488fc4664ed
Alexey Osipenko <aratak@anahoret.com>
2010-06-14 14:13:14 +0300

    removing dupliucate of deliver_plan_change.

--- app/models/owner/migrations.rb
+++ app/models/owner/migrations.rb
@@ -113,38 +113,7 @@ class Owner
 
     cancel_recurring
 
-    Mailer.deliver_plan_change(self)
     true
   end
   
 end
-# 
-# @owner.require_current_password
-# @owner.update_attributes(params[:preferences][:owner])
-# @message = ['preferences.updated']
-# 
-# @plan = params[:preferences][:owner][:plan]
-# @card = ExtCreditCard.new params[:preferences][:card]
-#
-# @plan_changed = @plan != @owner.plan
-# if @plan == 'professional' && (@owner.plan_changed? || !@card.number.blank?)
-#   @card.valid?
-#   if @owner.errors.empty? && @card.errors.empty?
-#     begin
-#       if @owner.plan_changed?
-#         @owner.set_plan(Plan::PROFESSIONAL, params)
-#         @message = ['plan.upgraded', {:plan_was => @owner.plan_was.name}]
-#       else
-#         @owner.set_card @card
-#       end
-#     rescue PaymentSystemError
-#       render_message I18n.t('plan.payment_error', 
-#         :contact_us => MessageKeywords.contact_us('contact us'), 
-#         :support => MessageKeywords.support_email)
-#     end
-#   end
-# end
-# 
-# unless @owner.errors.empty? && @card.errors.empty?
-#   render_errors :preferences_owner => @owner, :preferences_card => @card
-# end
