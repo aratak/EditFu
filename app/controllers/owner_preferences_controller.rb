68fb439e720b42f3b40c81550af071377f7f36ff
Sergey Kachan <sk@anahoret.com>
2010-06-16 14:48:10 +0300

    removed debug code.

--- app/controllers/owner_preferences_controller.rb
+++ app/controllers/owner_preferences_controller.rb
@@ -16,26 +16,7 @@ class OwnerPreferencesController < ApplicationController
     # @owner.build_card
     @owner.set_plan(params[:owner][:plan_id])
     @owner.update_attributes(params[:owner])
-    p "="*50
-    p @owner
-    p @owner.card
-    p "^"*50
     render_errors(:owner => @owner) unless @owner.save
-
-    # begin
-    #   @plan_changed = @owner.set_plan(params[:owner][:plan_id], params) if params[:preferences][:owner][:plan_id]
-    #   @owner.build_card(params[:preferences][:card]) if params[:preferences][:card]
-    # rescue PaymentSystemError
-    #   render_message I18n.t('plan.payment_error', 
-    #                         :contact_us => MessageKeywords.contact_us('contact us'), 
-    #                         :support => MessageKeywords.support_email)
-    # end
-    # unless @owner.save
-    #   @message = [I18n.t('plan.upgraded', :plan_was => @owner.plan_was.name)] if @plan_changed
-    #   @message = [I18n.t('credit_card.updated')] if @card
-    # else
-    #   render_errors :owner => @owner #, :preferences_card => @card
-    # end
   end
 
   def downgrade
