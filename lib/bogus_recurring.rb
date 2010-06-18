beca3b1c56ee708cd590131b0742c0a6b1cfb6f7
Alexey Osipenko <aratak@anahoret.com>
2010-06-18 11:15:50 +0300

    bodus recurring fix.

--- lib/bogus_recurring.rb
+++ lib/bogus_recurring.rb
@@ -2,7 +2,7 @@ class BogusRecurring
   def self.create(gateway, amount, owner, card)
   end
 
-  def self.update(gateway, owner)
+  def self.update(gateway, owner, card)
   end
 
   def self.cancel(gateway, owner)
