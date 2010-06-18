ff9ccb31014b8cb754ba28fcc10c2654c29bc748
Alexey Osipenko <aratak@anahoret.com>
2010-06-18 17:41:14 +0300

    plan has display_name now.

--- app/models/plan/fixture.rb
+++ app/models/plan/fixture.rb
@@ -1,11 +1,11 @@
 class Plan
   
   # there are list of all plans
-  create :id => 1, :name => "Trial", :price => BigDecimal.new("0")
-  create :id => 2, :name => "Free", :price => BigDecimal.new("0")
-  create :id => 3, :name => "UnlimitedTrial", :price => BigDecimal.new("0")
-  create :id => 4, :name => "Single", :price => BigDecimal.new("3")
-  create :id => 5, :name => "Professional", :price => BigDecimal.new("15")
+  create :id => 1, :name => "Trial",            :price => BigDecimal.new("0"),    :display_name => "Trial"
+  create :id => 2, :name => "Free",             :price => BigDecimal.new("0"),    :display_name => "Free"
+  create :id => 3, :name => "UnlimitedTrial",   :price => BigDecimal.new("0"),    :display_name => "Buddy"
+  create :id => 4, :name => "Single",           :price => BigDecimal.new("3"),    :display_name => "Single"
+  create :id => 5, :name => "Professional",     :price => BigDecimal.new("15"),   :display_name => "Pro"
   
   PAYMENTS = [SINGLE, PROFESSIONAL]
 
