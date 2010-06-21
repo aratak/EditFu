def2a07da915007fb3b7aa711dbfffcf182307a2
Alexey Osipenko <aratak@anahoret.com>
2010-06-21 16:33:50 +0300

    remove_column :users, :subscription_id

--- db/migrate/20100615090017_add_card.rb
+++ db/migrate/20100615090017_add_card.rb
@@ -6,9 +6,13 @@ class AddCard < ActiveRecord::Migration
       t.string :display_expiration_date
       t.string :subscription_id
     end
+    
+    remove_column :users, :subscription_id
+    
   end
 
   def self.down
     drop_table :cards
+    add_column :users, :subscription_id, :string, :limit => 13
   end
 end
