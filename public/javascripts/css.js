53cd5fa8c6047acfd5222b1d6f8874843ad68b25
Alexey Osipenko <aratak@anahoret.com>
2010-06-21 16:34:40 +0300

    flash design has been changed.

--- public/javascripts/css.js
+++ public/javascripts/css.js
@@ -4,7 +4,12 @@ function setInputValue(input, value) {
 }
 
 function hideFlash(item) {
-  $(item).up().hide()
+  // $(item).up().hide()
+  var action = function() {
+    $(item).up().morph('height: 0; display: none;')
+  }
+  
+  action()
 }
 
 // function toggleInputClass(input, value) {
