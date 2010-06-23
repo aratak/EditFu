bce2f8fad9399fbb377afc9fa1b0db6098ec9378
Alexey Osipenko <aratak@anahoret.com>
2010-06-23 14:57:30 +0300

    card.rb has been refactoried.

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,7 @@
 class Card < ActiveRecord::Base
 
+
+  @first_name = ""
   attr_accessor :first_name, :last_name, :expiration, 
                 :number, :verification_value, :zip, 
                 :credit_card
@@ -12,58 +14,67 @@ class Card < ActiveRecord::Base
   validates_presence_of :number
   validates_presence_of :verification_value
   validates_presence_of :zip
-
-
-  before_validation_on_update :update_recurring
-  before_validation_on_create :recurring
-  before_validation :set_credit_card
   
-  # before_save :set_card_fields
+  after_validation :set_credit_card
+  after_validation_on_create :recurring
+  after_validation_on_update :update_recurring
+  after_validation :subscription_validation
+
+  before_save :set_display_card_fields
   after_save :deliver_credit_card_changes
   before_destroy :cancel_recurring
 
-  
-  def set_credit_card
-    self.credit_card = ExtCreditCard.new(attributes_for_credit_card)
-  end
-  
-  def recurring
-    begin
-      PaymentSystem.recurring(self.owner, self)
-      set_card_fields
-    rescue PaymentSystemError
-      errors.add :credit_card, "is invalid"
-    end    
-  end
-  
-  def update_recurring
-    begin
-      PaymentSystem.update_recurring(self.owner, self)
-      set_card_fields
-    rescue PaymentSystemError
-      errors.add :credit_card, "is invalid"
-    end    
-  end
-  
-  def set_card_fields
-    self.display_number = credit_card.display_number                                  # 'XXXX-'*3 + number[-4..-1] 
-    self.display_expiration_date = Date.new(credit_card.year, credit_card.month, 1)   # self.expiration_date
-  end
-  
-  def cancel_recurring
-    PaymentSystem.cancel_recurring(self.owner)
-  end
-
-  
   def deliver_credit_card_changes
     Mailer.deliver_credit_card_changes(self.owner)
   end
 
   private
 
-  def validate
-    errors.add :credit_card, "is invalid" if credit_card.invalid?
+  def set_display_card_fields
+    self.display_number = self.credit_card.display_number
+    self.display_expiration_date = Date.new(credit_card.year, credit_card.month, 1)
   end
+
+  def set_credit_card
+    self.credit_card = ExtCreditCard.new(attributes_for_credit_card)
+    credit_card_validation
+  end
+  
+  
+  def credit_card_validation
+    errors.add :credit_card, "is invalid" if self.credit_card.invalid?
+  end
+  
+  def subscription_validation
+    errors.add(:subscription_id, "Required.") if self.subscription_id.nil? || self.subscription_id.empty?
+  end
+  
+  
+  def recurring
+    begin
+      PaymentSystem.recurring(self)
+      true
+    rescue PaymentSystemError
+      errors.add :credit_card, "is invalid"
+      false
+    end    
+  end
+  
+  def update_recurring
+    begin
+      PaymentSystem.update_recurring(self)
+      true
+    rescue PaymentSystemError
+      errors.add :credit_card, "is invalid"
+      false
+    end    
+  end
+  
+  def cancel_recurring
+    PaymentSystem.cancel_recurring(self)
+  end
+  
+
 
   def attributes_for_credit_card
     result = {}
@@ -73,7 +84,6 @@ class Card < ActiveRecord::Base
     end
     result
   end
-
 
 end
 
