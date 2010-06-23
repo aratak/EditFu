33adb87122ab3b75999fcfa2c07b88ecca343aca
Alexey Osipenko <aratak@anahoret.com>
2010-06-23 14:54:30 +0300

    changed API into AuthorizeNetRecurring.

--- lib/authorize_net_recurring.rb
+++ lib/authorize_net_recurring.rb
@@ -1,5 +1,6 @@
 class AuthorizeNetRecurring
-  def self.create(gateway, amount, owner, card)
+  def self.create(gateway, amount, card)
+    owner = card.owner
     response = gateway.recurring(amount, card.credit_card, 
       :interval => { :unit => :months, :length => 1 },
       :duration => { :start_date => owner.next_billing_date.strftime('%Y-%m-%d'), :occurrences => 9999 },
@@ -7,11 +8,12 @@ class AuthorizeNetRecurring
         :first_name => card.first_name, :last_name => card.last_name, :zip => card.zip
       }
     )
+
     card.subscription_id = response.params['subscription_id']
     raise PaymentSystemError, build_message(response) unless card.subscription_id
   end
 
-  def self.update(gateway, owner, card)
+  def self.update(gateway, card)
     response = gateway.update_recurring(
       :subscription_id => card.subscription_id,
       :credit_card => card.credit_card,
@@ -19,15 +21,17 @@ class AuthorizeNetRecurring
         :first_name => card.first_name, :last_name => card.last_name, :zip => card.zip
       }
     )
+
     raise PaymentSystemError, build_message(response) unless response.success?
   end
 
-  def self.cancel(gateway, owner)
-    if owner.card.subscription_id
-      response = gateway.cancel_recurring(owner.card.subscription_id)
-      raise PaymentSystemError, response.message unless response.success?
-      owner.card.subscription_id = nil
-    end
+  def self.cancel(gateway, card)
+    return false if card.subscription_id.nil?
+
+    response = gateway.cancel_recurring(card.subscription_id)
+    raise PaymentSystemError, response.message unless response.success?
+    card.subscription_id = nil
+    true
   end
 
   private
