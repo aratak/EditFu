2dff67e1c563c3f3035402048b70b94dd7fcefbd
Alexey Osipenko <aratak@anahoret.com>
2010-06-23 14:53:34 +0300

    tests_recuring has been changed: subscription_id is defined now.

--- lib/bogus_recurring.rb
+++ lib/bogus_recurring.rb
@@ -1,10 +1,11 @@
 class BogusRecurring
-  def self.create(gateway, amount, owner, card)
+  def self.create(gateway, amount, card)
+    card.subscription_id = rand(10000000).to_s
   end
 
-  def self.update(gateway, owner, card)
+  def self.update(gateway, card)
   end
 
-  def self.cancel(gateway, owner)
+  def self.cancel(gateway, card)
   end
 end
