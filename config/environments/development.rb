60fb377316a37d3b505223a1ed2cb05314e1f082
Alexey Osipenko <aratak@anahoret.com>
2010-06-29 15:41:25 +0300

    inaction_mailer gem has been added.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,8 @@ config.time_zone = 'Kyev'
 
 BASE_DOMAIN = 'gryadka.com'
 
+config.gem "inaction_mailer", :lib => 'inaction_mailer/force_load', :source => 'http://gemcutter.org'
+
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 
