32233237a249e464a150baf2f130a639cfef4590
Alexey Osipenko <aratak@anahoret.com>
2010-06-29 15:44:25 +0300

    Matt has been added as exeption recipient.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -40,6 +40,6 @@ PAYMENT_GATEWAY_OPTS = {
 }
 
 ExceptionNotification::Notifier.view_paths = ActionView::Base.process_view_paths(ExceptionNotification::Notifier.view_paths)
-ExceptionNotification::Notifier.exception_recipients = %w(aratak@anahoret.com)
+ExceptionNotification::Notifier.exception_recipients = %w(aratak@anahoret.com mattnorris@me.com)
 ExceptionNotification::Notifier.sender_address = %("EditFu Error" <errors@editfu.com>)
 ExceptionNotification::Notifier.email_prefix = "[EDITFU] "
