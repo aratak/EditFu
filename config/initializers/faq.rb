b3870f2b1b8f5b6b760eb4cfabd1d195ca664595
Alexey Osipenko <aratak@anahoret.com>
2010-07-13 09:52:41 +0300

    FAQ links updated.

--- config/initializers/faq.rb
+++ config/initializers/faq.rb
@@ -1,12 +1,12 @@
 FAQS = {
   :default     => "http://www.takeastep.me/editfu-faq/",
   
-  :add_pages        => "http://www.takeastep.me/editfu-faq/",
-  :add_sites        => "http://www.takeastep.me/editfu-faq/",
+  :add_pages        => "http://www.takeastep.me/editfu-faq/basics/how-do-i-add-a-page.html",
+  :add_sites        => "http://www.takeastep.me/editfu-faq/basics/how-do-i-add-a-site.html",
   :choose_sites     => "http://www.takeastep.me/editfu-faq/",
   
-  :add_editors      => "http://www.takeastep.me/editfu-faq/",
-  :preferences      => "http://www.takeastep.me/editfu-faq/",
+  :add_editors      => "http://www.takeastep.me/editfu-faq/editors/how-do-i-invite-an-editor.html",
+  :preferences      => "http://www.takeastep.me/editfu-faq/accounts-billing/",
   :choose_editors   => "http://www.takeastep.me/editfu-faq/",
   :no_content       => "http://www.takeastep.me/editfu-faq/"
 }
