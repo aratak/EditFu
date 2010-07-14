e31bfce5e22e50a2b8ade5b9264a2d55e5877f62
Alexey Osipenko <aratak@anahoret.com>
2010-07-14 17:17:02 +0300

    Default supscription has been added.

---
+++ db/migrate/20100714141122_add_default_subscription.rb
@@ -0,0 +1,16 @@
+class AddDefaultSubscription < ActiveRecord::Migration
+  def self.up
+    Owner.all.each do |owner|
+      if owner.subscriptions.empty?
+        owner.subscriptions.create :starts_at => owner.confirmed_at,
+                             :ends_at => owner.confirmed_at + owner.plan.period.month,
+                             :price => owner.plan.price,
+                             :plan => owner.plan
+      end
+    end
+  end
+
+  def self.down
+    Subscription.destroy_all
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100714081102) do
+ActiveRecord::Schema.define(:version => 20100714141122) do
 
   create_table "audits", :force => true do |t|
     t.integer  "auditable_id"
