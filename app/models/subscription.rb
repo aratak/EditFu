3e3366929c97f8b3caa14a9aa573d57b41a3ea65
Alexey Osipenko <aratak@anahoret.com>
2010-07-15 12:16:02 +0300

    subscription form fixed: price in dollars, removed general condition subscription_is_possible

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,12 +2,14 @@ class Subscription < ActiveRecord::Base
   
   default_scope :order => "ends_at"
 
+  attr_reader :price_in_dollars
+
   belongs_to :owner
   belongs_to :plan
   
   validates_presence_of :starts_at
   validates_presence_of :ends_at
-  validates_presence_of :price
+  validates_presence_of :price_in_dollars
   validates_presence_of :owner_id
   
   before_create :close_previous_subscirption
@@ -15,6 +17,16 @@ class Subscription < ActiveRecord::Base
   def close_previous_subscirption
     self.owner.close_latest_subscription
   end
+  
+  def price_in_dollars
+    self.price / 100
+  end
+
+  def price_in_dollars= val
+    p "="*50
+    self.price = (val.to_i * 100).to_i
+  end
+
   
   # def set_plan
   #   self.plan = self.owner.plan if self.plan.nil?
