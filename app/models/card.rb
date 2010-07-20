773bd4c15684cfe7202346ca18af2b47dd60440b
Alexey Osipenko <aratak@anahoret.com>
2010-07-20 16:43:29 +0300

    display_expiration_date updating is fixed.

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,7 +1,7 @@
 class Card < ActiveRecord::Base
   FIELDS = [:first_name, :last_name, :expiration, :number, :verification_value, :zip]
 
-  attr_accessor *(FIELDS + [:credit_card, :display_expiration_date] - [:expiration])
+  attr_accessor *(FIELDS + [:credit_card] - [:expiration])
 
   belongs_to :owner
 
@@ -31,30 +31,15 @@ class Card < ActiveRecord::Base
     self.owner.plan.price unless owner.nil?
   end
   
-  # def display_expiration_date= val
-  #   write_attribute :display_expiration_date, val
-  #   self.expiration = display_expiration_date.strftime("%m/%Y")
-  # end
-  
   def expiration
     display_expiration_date.strftime("%m/%Y")
   end
   
-  # def expiration= val
-  #   display_expiration_date = 
-  # end
-
   private
 
   def set_display_card_fields
     return true if !changed?
     self.display_number = self.credit_card.display_number
-    
-    # begin
-    #   self.display_expiration_date = Date.new(credit_card.year, credit_card.month, 1)
-    # rescue ArgumentError
-    #   errors.add :expiration, "is invalid"
-    # end
   end
 
   def set_credit_card
