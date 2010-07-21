2c4c3ad4d527d9b8afd767bc9d511c9485245efd
Alexey Osipenko <aratak@anahoret.com>
2010-07-21 13:42:18 +0300

    mailer field "from" is fixed.

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -1,88 +1,88 @@
 class Mailer < ActionMailer::Base
   def signup(user)
-    setup   user
+    setup   user, "sales@editfu.com"
     subject "Welcome to EditFu. Now meet the ninjas."
   end
 
   def editor_invitation(user)
-    setup   user
+    setup   user, "noreply@editfu.com"
     subject "Content editor invitation."
   end
 
   def reset_password(user)
-    setup   user
+    setup   user, "noreply@editfu.com"
     subject "EditFu password reset"
   end
 
   def plan_change(user)
-    setup   user
+    setup   user, "billing@editfu.com"
     subject 'EditFu plan change.'  
   end
 
   def credit_card_changes(user)
-    setup   user
+    setup   user, "billing@editfu.com"
     subject 'EditFu credit card changes.'
   end
 
   def account_cancellation(user)
-    setup   user
+    setup   user, "sales@editfu.com"
     subject 'EditFu account cancellation.'
   end
 
   def owner_subdomain_changes(user)
-    setup   user
+    setup   user, "noreply@editfu.com"
     subject 'EditFu subdomain changes.'
   end
 
   def editor_subdomain_changes(owner, editor)
-    setup   editor
+    setup   editor, "noreply@editfu.com"
     subject 'Content editing system address changes.'
     body    :owner => owner, :editor => editor
   end
 
   def owner_email_changes(user)
-    setup   user
+    setup   user, "noreply@editfu.com"
     subject 'EditFu email address.'
   end
 
   def editor_email_changes(user)
-    setup   user
+    setup   user, "noreply@editfu.com"
     subject 'Content editing system email address change.'
   end
 
   def content_update_error(editor, message)
-    setup   editor.owner
+    setup   editor.owner, "noreply@editfu.com"
     subject 'EditFu content update error.'
     body    :editor => editor, :owner => editor.owner, :message => message
   end
 
   def card_expiration(user)
-    setup   user
+    setup   user, "billing@editfu.com"
     subject 'EditFu billing credit card.'
   end
 
   def card_has_expired(user)
-    setup   user
+    setup   user, "billing@editfu.com"
     subject 'EditFu billing credit card.'
   end
 
   def trial_expiration(user)
-    setup   user
+    setup   user, "billing@editfu.com"
     subject 'EditFu trial.'
   end
   
   def trial_expiration_reminder(user)
-    setup   user
+    setup   user, "billing@editfu.com"
     subject 'EditFu trial reminder.'
   end
 
   def hold(user)
-    setup   user
+    setup   user, "billing@editfu.com"
     subject 'EditFu account hold'
   end
   
   def tomorrow_holded_status
-    setup   user
+    setup   user, "billing@editfu.com"
     subject 'EditFu account hold'
   end
   
@@ -96,8 +96,8 @@ class Mailer < ActionMailer::Base
 
   private
 
-  def setup(user)
-    from         "edit.fu.cms@gmail.com"
+  def setup(user, email="edit.fu.cms@gmail.com")
+    from         email
     recipients   user.email
     content_type 'text/html'
     body         :user => user
