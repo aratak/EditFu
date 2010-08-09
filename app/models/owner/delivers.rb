67fd6ddcb07d4ebe9f921421b28c8c1b7d2525b6
Alexey Osipenko <aratak@anahoret.com>
2010-08-09 15:56:37 +0300

    Hold report has been changed: added new paragraph about the todays new subscriptions.

--- app/models/owner/delivers.rb
+++ app/models/owner/delivers.rb
@@ -14,11 +14,13 @@ class Owner
   def self.deliver_next_week_report
     weeks_owners = Subscription.ends_earlier_than(1.week).map(&:owner)
     days_owners = Subscription.ends_earlier_than(1.day).map(&:owner)
-    Mailer.deliver_hold_report(:weeks_owners => weeks_owners, :days_owners => days_owners)
-    
-    Subscription.ends_todays.each do |owner|
+    todays_owners = Subscription.ends_todays
+
+    todays_owners.each do |owner|
       owner.create_next_subscription
     end
+
+    Mailer.deliver_hold_report(:weeks_owners => weeks_owners, :days_owners => days_owners, :todays_owners => todays_owners)
   end
   
   def send_confirmation_instructions
