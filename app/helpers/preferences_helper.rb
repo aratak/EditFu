2d7a03f416f26d8ceef9b3df044ff5c2a789ed7f
Alexey Osipenko <aratak@anahoret.com>
2010-08-09 17:47:43 +0300

    Fixed compatibility for the future version of ruby,

--- app/helpers/preferences_helper.rb
+++ app/helpers/preferences_helper.rb
@@ -1,7 +1,7 @@
 module PreferencesHelper
 
   def set_elements_max_height container="div.plan-wrapper", children="div.plan-label"
-    return javascript_tag "setElementsHeight('#{container}', '#{children}')"
+    return javascript_tag("setElementsHeight('#{container}', '#{children}')")
   end
 
 end
