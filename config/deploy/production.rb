b198182eecf79af9390acd2c4461eafab32edaf7
Alexey Osipenko <aratak@anahoret.com>
2010-08-13 15:19:34 +0300

    dev and production environments are without questions now: default tag/branch has been set.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,4 @@
 set :deploy_to, "/var/www/rails/#{application}/production"
-# set :branch, "stable"
-
-set :branch do
-  default_branch = "stable"
-
-  branch = Capistrano::CLI.ui.ask "Branch to deploy (make sure to push the tag first): [#{default_branch}] "
-  branch = default_branch if branch.empty?
-  branch
-end
-
+set :branch, "stable"
 set :rails_env, "production"
 
